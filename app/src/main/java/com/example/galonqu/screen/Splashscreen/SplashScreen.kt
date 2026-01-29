18fe53dc440084f85592c3f3e68805bbce5a6c2c
unknown <anampict@gmail.com>
2026-01-29 12:52:28 +0700

    init splashScreen

--- app/src/main/java/com/example/galonqu/screen/Splashscreen/SplashScreen.kt
+++ app/src/main/java/com/example/galonqu/screen/Splashscreen/SplashScreen.kt
@@ -1,16 +1,28 @@
 package com.example.galonqu.screen.Splashscreen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.galonqu.R
 
 @Composable
 fun SplashScreen() {
-    Box(modifier = Modifier.fillMaxSize().background(color = Color(0xff03AC0E))) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color(0xff03AC0E)), contentAlignment = Alignment.Center) {
+        Image(
+            painter = painterResource(R.drawable.logo),
+            contentDescription = "",
+            contentScale = ContentScale.FillBounds
+        )
 
 
     }
